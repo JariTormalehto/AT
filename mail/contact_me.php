e73141f74c6b987e177826946410846abad99a52
Unknown <jari.torm@hotmail.com>
2020-11-03 23:20:23 +0200

    1st changes

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -2,6 +2,7 @@
 // Check for empty fields
 if(empty($_POST['name'])  		||
    empty($_POST['email']) 		||
+   empty($_POST['telefon']) 		||
    empty($_POST['message'])	||
    !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
    {
@@ -12,13 +13,19 @@ if(empty($_POST['name'])  		||
 $name = $_POST['name'];
 $email_address = $_POST['email'];
 $message = $_POST['message'];
+$telefon = $_POST['telefon'];
 	
 // Create the email and send the message
-$to = 'yourname@yourdomain.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "Website Contact Form:  $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nMessage:\n$message";
-$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";	
+$recipients = array{
+"taritvo@taritvo.ee",
+"reili@taritvo.ee",
+"jari@taritvo.ee"};
+
+$to = implode(',' $recipients); // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$email_subject = "Kodulehe kontaktvorm  $name";
+$email_body = "Uus sõnum kodulehe kontaktvormilt.\n\n"."Kontakti info:\n\nNimi: $name\n\nE-mail: $email_address\n\nTelefon: $telefon\n\nMessage:\n$message";
+$headers = "From: Koduleht@taritvo.ee\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers .= "Vasta: $email_address";	
 mail($to,$email_subject,$email_body,$headers);
 return true;			
 ?>
